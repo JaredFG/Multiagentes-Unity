0f56bdb4344dce97ad376213e1236bad95fd8277
Edurdgzl <A01749381@itesm.mx>
2021-12-01 20:25:19 -0600

    Actualización del archivo de conexión
    Mejoras al código.

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@ import json, logging, os, atexit
 from CarModel2 import *
 
 app = Flask(__name__, static_url_path='')
-#app = Flask("Urban Mobility")
 model = CarModel(15, 20, 20)
 
 def get_key (elem: tuple) -> int:
@@ -16,7 +15,6 @@ port = int(os.getenv('PORT', 8000))
 
 @app.route('/CarAgentsPositions', methods=["GET","POST"])
 def getCarPositions():
-
     cars, car_pos = [], []
     for (agents, x, z) in model.grid.coord_iter():
         if len(agents) > 0:
@@ -26,22 +24,7 @@ def getCarPositions():
     cars.sort(key=get_key)
     car_pos = get_second(cars)
     model.step()
-    #return jsonify({"data": car_pos})
     return json.dumps(car_pos)
-
-
-@app.route('/TrafficLightsAgentsPositions', methods=["GET","POST"])
-def getLightsPositions():
-    lights, light_pos = [], []
-    for (agents, x, z) in model.grid.coord_iter():
-        if len(agents) > 0:
-            for a in agents:
-                if a.agent_type == 1:
-                    lights.append(a.unique_id, {"x": x, "y": 0, "z": z})
-    lights.sort(key=get_key)
-    light_pos = get_second(lights)
-    return jsonify({"positions": light_pos})
-
 
 @app.route('/TrafficLightsAgentsStates', methods=["GET","POST"])
 def getLightsStates():
@@ -54,9 +37,7 @@ def getLightsStates():
     lights.sort(key=get_key)
     light_state = get_second(lights)
     model.step()
-    #return jsonify({"states": light_state})
     return json.dumps(light_state)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=port, debug=True)
-    #app.run(host="localhost", port=8585, debug=True)
